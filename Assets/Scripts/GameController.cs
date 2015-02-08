b6af79c601572d5459dbce3a1e22016af9747dea
Draydan <yuokol@yandex.ru>
2015-02-08 21:52:30 +0300

    Initial commit: implementation of couple lessons of tutorial

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public static GUIText debugText;
+	public string debugTextValue;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public static void Trace(string msg){
+		debugText.text = msg;
+	}
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	float touchX, touchY;
+	//public GUIText debugText;
+
+	void FixedUpdate()	{
+		const float speedModul = 0.1f;
+		// Loop through the touches looking for a finger on the right button.
+		//for (int t = 0; t < Input.touchCount; t++) 
+		if(Input.touchCount==1)
+		{
+			int t = 0;
+			if (Input.touches [t].phase == TouchPhase.Began)
+			{
+				touchX = Input.touches[t].position.x;
+				touchY = Input.touches[t].position.y;
+			}
+			if( Input.touches [t].phase == TouchPhase.Moved || 
+					Input.touches [t].phase == TouchPhase.Stationary) 
+			{
+				float  nowTouchX, nowTouchY, touchLenX, touchLenY;
+				nowTouchX = Input.touches[t].position.x;
+				nowTouchY = Input.touches[t].position.y;
+				touchLenX = nowTouchX - touchX ;
+				touchLenY = nowTouchY - touchY ;
+				float touchLen = Mathf.Sqrt(Mathf.Pow (touchLenX , 2) + Mathf.Pow (touchLenY, 2) );
+
+				rigidbody.velocity += new Vector3( touchLenX * speedModul / touchLen, touchLenY * speedModul / touchLen, 0);
+				//Vector2 inputPosition = new Vector2 ((Input.touches[t].position.x - rigidbody.position.x)/100, (Input.touches[t].position.y - rigidbody.position.y)/100);
+				//rigidbody.velocity = inputPosition;
+
+				//angle = angle*Mathf.PI/180;
+				//rigidbody.position = new Vector2 (Input.touches[t].position.x, Input.touches[t].position.y);
+
+				/*
+				float angle = 2*Mathf.PI + Mathf .Atan2(Input.touches[t].position.x - rigidbody.position.x, Input.touches[t].position.y - rigidbody.position.y);
+				rigidbody.velocity  = new Vector3 (Mathf.Cos (angle) * speedModul , Mathf.Sin (angle) * speedModul, 0);
+				*/
+
+				//StackTraceUtility stu = new StackTraceUtility 
+				//if (rigidbody.position.x > camera.po
+				//debugText.text = string.Format("{0} - {1} - {2}", rigidbody.velocity.x, rigidbody.velocity.y, rigidbody.velocity.z);
+				GameController.Trace(string.Format("{0} - {1} - {2}", rigidbody.velocity.x, rigidbody.velocity.y, rigidbody.velocity.z));
+			}
+		}
+
+		//rigidbody.velocity = new Vector3 (1, 1,0);
+
+/*		if (Input.mousePresent) {
+			Vector2 inputPosition = new Vector2 (Input.mousePosition.x - rigidbody.position.x, Input.mousePosition.y - rigidbody.position.y);
+			//Vector2 rocketPosition = 
+			
+			rigidbody.velocity = inputPosition;
+		}
+		*/
+
+		rigidbody.rotation.SetLookRotation (rigidbody.velocity);		
+	}
+}
